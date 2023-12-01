c97314c6b91417a0095e291a2e4b6441daaa17b8
ActuarioPedro <pserranor@prevemseguros.com.mx>
2023-12-01 14:26:34 -0600

    Pusimos función que calcula los elementos de la sucesion de Fibonnaci

--- CODIGO_PRUEBA_GITHUB.R
+++ CODIGO_PRUEBA_GITHUB.R
@@ -15,4 +15,16 @@ combinaciones_n_en_m <- function(n,m){
   return(n_factorial(n)/n_factorial(m-n)*factorial(m))
 }
 
-#Saludos Banda!!
+
+###################################################################################
+
+# Definir una función recursiva para la sucesión de Fibonacci
+fibonacci <- function(n) {
+  # Caso base: si n es 0 o 1, devolver n
+  if (n == 0 || n == 1) {
+    return(n)
+  } else {
+    # Caso recursivo: devolver la suma de los dos términos anteriores
+    return(fibonacci(n - 1) + fibonacci(n - 2))
+  }
+}
